d68a97f7f37d7db051b12e13e78c564a789884ec
411022938 <00112233445566778899.asdfghjkl@gmail.com>
2022-10-27 18:26:09 +0800

    make it better

--- index.py
+++ index.py
@@ -5,12 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python網頁</h1>"
-    homepage += "<a href=/about>個人求職自傳履歷網頁</a><br>"
-    return homepage
-
-@app.route("/about")
-def about():
     return render_template("index.html")
 
 if __name__ == "__main__":
