4cb23ffa21cdca7c15792f010a2dc5fbd28ea88d
411022938 <00112233445566778899.asdfghjkl@gmail.com>
2022-10-27 17:33:15 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>楊子青Python網頁</h1>"
+    homepage += "<a href=/about>個人求職自傳履歷網頁</a><br>"
+    return homepage
+
+@app.route("/about")
+def about():
+    return render_template("aboutme.html")
+
+if __name__ == "__main__":
+    app.run()
